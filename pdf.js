c1683cc7b79eaa5dc276c5061481a627885c9b3e
Adnan Raghib <adnanraghib@192.168.1.5>
2022-05-18 10:29:38 +0530

    pdf

--- pdf.js
+++ pdf.js
@@ -2,6 +2,18 @@ const fs = require("fs");
 const path = require("path");
 const handlebars = require("handlebars");
 var pdff = require('html-pdf');
+var AWS = require('aws-sdk');
+// Set the region 
+
+const SESConfig = {
+  region: "us-east-1",
+  "apiVersion": "2012-11-05",
+  "accessKeyId": "AKIARZIOHHWYSTTVJUH4",
+  "secretAccessKey": "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",
+}
+
+AWS.config.update(SESConfig);
+const s3 = new AWS.S3();
 
 exports.generatePdf = generatePdf = () => {
   try {
@@ -40,9 +52,24 @@ exports.generatePdf = generatePdf = () => {
             path: 'invoice.pdf'
         }
 
-        pdff.create(template, options).toFile('./businesscard.pdf', function(err, res) {
-            if (err) return console.log(err);
-            console.log(res); // { filename: '/app/businesscard.pdf' }
+        // pdff.create(`${template}`, options).toFile('./businesscard.pdf', function(err, res) {
+        //     if (err) return console.log(err);
+        //     console.log(res); // { filename: '/app/businesscard.pdf' }
+        // });
+        pdff.create(template).toStream(function(err, stream){
+          //stream.pipe(fs.createWriteStream('foo1.pdf'));
+          const params = {
+                        Key: 'foo3.pdf',
+                        Body: stream,
+                        Bucket: 'pdfdocsbill',
+                        ContentType: 'application/pdf',
+                    };
+          s3.upload(params, (err, res) => {
+                        if (err) {
+                            console.log(err, 'err');
+                        }
+                        console.log(res, 'res');
+                    });
         });
     })();
   } catch (err) {
